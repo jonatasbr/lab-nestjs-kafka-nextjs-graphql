11ac48cd629bade96480bc10b002b818d10a0c42
Jonatas Sousa <jonatasbraz@gmail.com>
2022-04-26 00:08:00 -0300

    web - terminando autenticacao

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,29 +1,26 @@
-import { getAccessToken, useUser } from "@auth0/nextjs-auth0";
+import { getSession } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
-import Link from "next/link";
 
 export default function Home() {
-  const { user } = useUser();
-
-  return (
-    <div>
-      <h1>hello, lab</h1>
-
-      <br/>
-      { JSON.stringify(user, null, 3) }
-      <br/>
-
-      <Link href="/api/auth/login">
-        <a>login</a>
-      </Link>
-    </div>
-  )
+  return null;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const token = getAccessToken(req, res);
-  console.log(token);
+  const session = getSession(req, res);
+  
+  if (session) {
+    return {
+      redirect: {
+        destination: '/app',
+        permanent: false,
+      }
+    }
+  }
+
   return {
-    props: {}
+    redirect: {
+      destination: '/api/auth/login',
+      permanent: false,
+    }
   }
 }
